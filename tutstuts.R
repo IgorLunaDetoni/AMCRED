bb1240ac6e28e56c03247d964013fe60f79a9547
Igor Luna <igorlunad@gmail.com>
2023-04-20 08:56:31 -0300

    Criação dos modelos e atualização

--- tutstuts.R
+++ tutstuts.R
@@ -2,24 +2,33 @@
 
 library(tidyverse)
 library(parsnip)
-
-
-
-
-
+library(rsample)
+library(dplyr)
+library(recipes)
+library(themis)
+library(tune)
+library(yardstick)
+library(dplyr)
 # Pegar os dados do Aruã --------------------------------------------------
 
-
+df<-read.csv('BLUSOL/BLUSOL/df.csv')
 
 
 # Modelo classificação ----------------------------------------------------
+
+
+# SMOTE -------------------------------------------------------------------
+
+df$DEFAULT<-as.factor(df$DEFAULT)
+df1 <- themis::smote(df,var = "DEFAULT", k = 6, over_ratio = 1)
+prop.table(table(df1$DEFAULT))
 
 
 
 # Split -------------------------------------------------------------------
 
 
-split <- initial_split(f, prop = 0.75)
+split <- initial_split(df1, prop = 0.75)
 x_train <- training(split)
 x_test <- testing(split)
 
@@ -27,10 +36,8 @@ x_test <- testing(split)
 ## Recipes
 
 
-recipe1 <- recipes::recipe(clusters~.,x_train) %>% 
-  step_rm (c("NOME_DO_PRODUTO","CNPJ_CLIENTE_PJ")) %>%
+recipe1 <- recipes::recipe(DEFAULT~.,x_train) %>% 
   step_unknown(0) %>% 
-  step_dummy(c("PORTE_CLIENTE_PJ","classification"),one_hot = TRUE) %>% 
   prep()
 
 
@@ -41,15 +48,16 @@ tst_proc <- bake(receita_prep, new_data = x_test)
 
 
 #Prop teste
-prop.table(table(tst_proc$clusters))
+prop.table(table(tst_proc$DEFAULT))
 
 
 #Prop treino
-prop.table(table(tr_proc$clusters))
+prop.table(table(tr_proc$DEFAULT))
+
 
 #### K folds validação cruzada
 
-receita2 <- recipe(clusters~.,tr_proc) %>% prep()
+receita2 <- recipe(DEFAULT~.,tr_proc) %>% prep()
 
 
 cv_split<-vfold_cv(tr_proc, v=5)
@@ -83,14 +91,14 @@ boost_grid %>%
   collect_metrics() %>% 
   head()
 best<-boost_grid %>% 
-  select_best("accuracy")
+  select_best("kap")
 
 
 
 ### Finalizando Boost
 
 boost_fit <- finalize_model(boost_tree_xgboost_spec, parameters = best) %>% 
-  fit(clusters~.,tr_proc)
+  fit(DEFAULT~.,tr_proc)
 boost_fit
 
 saveRDS(boost_fit,"xgboost.rda")
@@ -98,12 +106,99 @@ saveRDS(boost_fit,"xgboost.rda")
 
 xc<-boost_fit %>% 
   predict(new_data = tst_proc) %>% 
-  mutate(observado = tst_proc$clusters,
+  mutate(observado = tst_proc$DEFAULT,
          modelo = "XGBoost Tuned")
 
-resultado_xg <- fitted %>% 
+resultado_xg <- xc %>% 
   group_by(modelo) %>% 
   metrics(truth = observado, estimate = .pred_class)
 
 resultado_xg
 
+
+
+# Random forest -----------------------------------------------------------
+
+rand_forest_ranger_spec <-
+  rand_forest(mtry = tune(), min_n = tune()) %>%
+  set_engine('ranger') %>%
+  set_mode('classification')
+
+doParallel::registerDoParallel()
+
+boost_grid<-tune_grid(rand_forest_ranger_spec, 
+                      receita2,
+                      resamples = cv_split,
+                      grid = 10,
+                      metrics = metric_set(accuracy,kap))
+
+boost_grid %>% 
+  collect_metrics() %>% 
+  head()
+best<-boost_grid %>% 
+  select_best("kap")
+
+boost_fit2 <- finalize_model(rand_forest_ranger_spec, parameters = best) %>% 
+  fit(DEFAULT~.,tr_proc)
+boost_fit2
+
+saveRDS(boost_fit2,"randomforest.rda")
+
+fitted<-boost_fit2 %>%
+  predict(new_data = tst_proc) %>%
+  mutate(observado = tst_proc$DEFAULT,
+         modelo = "Random Tuned")
+
+xc<-rbind(xc,fitted)
+
+resultado_fin <- xc %>% 
+  group_by(modelo) %>%
+  dplyr::filter(modelo=="Random Tuned") %>% 
+  metrics(truth = observado, estimate = .pred_class)
+
+resultado_fin
+
+
+
+# SVM ---------------------------------------------------------------------
+
+svm_linear_kernlab_spec <-
+  svm_linear(cost = tune(), margin = tune()) %>%
+  set_engine('kernlab') %>%
+  set_mode('classification')
+
+
+boost_grid<-tune_grid(svm_linear_kernlab_spec, 
+                      receita2,
+                      resamples = cv_split,
+                      grid = 10,
+                      metrics = metric_set(accuracy,kap))
+
+boost_grid %>% 
+  collect_metrics() %>% 
+  head()
+best<-boost_grid %>% 
+  select_best("kap")
+
+boost_fit2 <- finalize_model(svm_linear_kernlab_spec, parameters = best) %>% 
+  fit(DEFAULT~.,tr_proc)
+boost_fit2
+
+saveRDS(boost_fit2,"SVM.rda")
+
+
+
+
+fitted<-boost_fit2 %>%
+  predict(new_data = tst_proc) %>%
+  mutate(observado = tst_proc$DEFAULT,
+         modelo = "SVM Tuned")
+
+xc<-rbind(xc,fitted)
+
+resultado <- xc %>% 
+  group_by(modelo) %>%
+  dplyr::filter(modelo=="SVM Tuned") %>% 
+  metrics(truth = observado, estimate = .pred_class)
+
+resultado
